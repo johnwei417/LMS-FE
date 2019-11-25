9328a161d1a76b75bd676fc6d87ca1f772a0ddfe
CK Wong <kvnwong0@gmail.com>
2019-11-24 19:09:14 -0500

    Updated video module
    - now supports going through the whole JSON form

--- src/component/vue-components/modules.vue
+++ src/component/vue-components/modules.vue
@@ -3,72 +3,115 @@
     <!-- Video -->
     <div class="row" v-if="showVideo">
         <div style="max-width:1000px;" class="centered">
-            <video width="1000" controls @ended="toAssesment()" :autoplay="autoplay" :key="part">
+            <video width="1000" controls @ended="checkState()" :autoplay="autoplay" :key="videoSrc">
                 <source :src="videoSrc" type="video/mp4" >
             </video>
-            <button v-if="showNext" @click="nextVideo()">NEXT</button>
+            <!--<button v-if="showNext" @click="nextVideo">NEXT</button>-->
         </div>
     </div>
     <!-- Quiz -->
     <div class="row" v-if="showAssesment">
-        <div class="text-center"> 
-            <p>{{assesment}}</p>
-            <button @click="toVideo(1)">GOT IT RIGHT</button>
-            <button @click="toVideo(0)">GOT IT WRONG</button>
+        <div class="text-center">
+            <h3>{{title}}</h3>
+            <div v-for="(question, num) in body" :key="num">
+                <h4>{{question.question}}</h4>
+                <div v-for="(item, index) in question.selection" :key="index">
+                    <input type="radio" v-model="studentAnswers[num]" :value="item.display">{{item.display}}
+                </div>
+            </div>
+            <button @click="checkAnswer()">Submit</button>
+        </div>
+    </div>
+    <!-- End -->
+    <div class="row" v-if="showEnd">
+        <div class="text-center">
+            <h2>THE END</h2>
+            <h4>Total Score: 100%</h4>
         </div>
     </div>
   </div>
 </template>
 
 <script>
+import VIDEO_QUIZ_COMPONENT from './video_quiz_src.json'
+
   export default {
     data () {
         return {
-            videoSrc: '/src/resources/Blackmer1.mp4',
-            assesmentCount: 0,
-            assesment: 'FIRST ASSESMENT',
+            videoSrc: null,
+            title: null,
+            body: null,
+            progressCount: 0,
             showVideo: true,
             showAssesment: false,
-            currentStatus: false,
             autoplay: false,
-            showStatusVideo: false,
             showNext: false,
-            part: 0,
-            showVideo2: false
+            showVideo2: false,
+            showEnd: false,
+            studentAnswers: [],
+            video_quiz: VIDEO_QUIZ_COMPONENT.videos
         }
     },
+    created () {
+        this.videoSrc = this.video_quiz[this.progressCount].src
+        this.title = this.video_quiz[this.progressCount].title
+    },
     methods: {
-        toAssesment(){
-            if (!this.currentStatus) {
+        checkAnswer(){
+            console.log(this.studentAnswers)
+            console.log(this.body)
+            // assume correct at first
+            let correctStatus = true
+
+            // check every questions
+            for(let i = 0; i < this.body.length; i++) {
+                if (this.studentAnswers[i] != this.body[i].answer) {
+                    correctStatus = false
+                }
+            }
+
+            if (correctStatus) {
+                this.progressCount += 1
+            } else {
+                this.progressCount += 2
+            }
+            
+            this.nextVideo()
+        },
+        checkState(){
+            if (this.progressCount == 8) {
+                // end module
+                this.showVideo = false
+                this.autoplay = false
+                this.showEnd = true
+
+            } else if (this.progressCount == 2 || this.progressCount == 6) {
+                // show video
+                this.progressCount += 2
+                this.nextVideo()
+            } else {
+                if (this.progressCount == 3 || this.progressCount == 7) {
+                    // repeat previous quiz again 
+                    this.progressCount -= 2
+                } else {
+                    // progress next 
+                    this.progressCount++
+                }
+
+                // show quiz
+                this.title = this.video_quiz[this.progressCount].title
+                this.body = JSON.parse(this.video_quiz[this.progressCount].body).quiz
                 this.showVideo = false
                 this.showAssesment = true
-            } else {
-                console.log('go it right!')
-                this.showNext = true
             }
+
         },
-        toVideo(status){
-            this.autoplay = true
-            if (status) {
-                this.currentStatus = true
-                // got right
-                this.videoSrc = '/src/resources/Blackmer2.mp4'
-                this.assesment = 'Which fraction represents half of a candy bar: a) 1/1, b) 1/2, c) 2/1, or d) 2/2.'
-            } else {
-                this.currentStatus = false
-                // got wrong
-                this.assesment = 'Alternative slide #2'
-                this.videoSrc = '/src/resources/Blackmer3.mp4'
-            }
-            this.showVideo = true
+        nextVideo() {
+            this.videoSrc = this.video_quiz[this.progressCount].src
+            this.title = this.video_quiz[this.progressCount].title
             this.showAssesment = false
-        },
-        nextVideo () {
-            this.currentStatus = false
-            this.videoSrc = '/src/resources/Blackmer4.mp4'
-            this.showNext = false
-            this.part++
-            console.log('NEXT')
+            this.showVideo = true
+            this.autoplay = true
         }
     }
   }
