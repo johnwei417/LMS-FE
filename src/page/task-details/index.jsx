c206a9237cdf8925994410cc5f968f7cc9d11557
John Wei <41243908+johnwei417@users.noreply.github.com>
2019-12-06 21:48:35 -0500

    tasks

--- src/page/task-details/index.jsx
+++ src/page/task-details/index.jsx
@@ -33,32 +33,12 @@ class TargetDetail extends React.Component{
     componentDidMount(){
         this.loadStudentListInTarget();
         this.checkLogin();
-        this.loadTaskList();
     }
 
     checkLogin(){
         if(localStorage.getItem("userInfo") === null){
         window.location.href = '/login';
         }
-    }
-
-    handleChange(event) {
-        this.setState({taskID: event.target.value});
-    }
-
-    loadTaskList(){
-        let Info = {};
-        Info.api_token = this.state.api_token;
-        Info.targetID = this.state.targetID;
-        _class.getTaskList(Info).then(res => {
-            this.setState({
-                taskList : res.tasks,
-               
-            });
-        }, errMsg =>{
-            _mm.errorTips(errMsg);
-        });
-
     }
 
 
@@ -80,59 +60,16 @@ class TargetDetail extends React.Component{
         }
     }
 
-    onInputChange(e){
-        let inputValue  = e.target.value,
-            inputName   = e.target.name;
-        if(inputName == "selected"){
-            this.setState({
-                selected : this.state.selected.concat(inputValue)
-            });
-            console.log(inputValue);
-        }else{ this.setState({
-            [inputName] : inputValue
-        });
-    }
-    }
 
-    onInputKeyUp(e){
-        if(e.keyCode === 13){
-            this.onSubmit();
-        }
-    }
-
-    onSubmit(){
-     let loginInfo = {
-        api_token: this.state.api_token,
-        userID: this.state.userID,
-        taskID: this.state.taskID
-        };
-        console.log(this.state.selected);
-       let data = {
-            "task" : {
-                   "class_id" : this.state.classID,
-                   "students_id" : this.state.selected
-               }
-        }
-    
-        _class.assignTask(loginInfo, JSON.stringify(data)).then((res)=>{
-             _mm.successTips(res.message);
-        
-        }, (errMsg) => {
-            _mm.errorTips(errMsg.message);
-        });
-
-    }
+   
 
     loadStudentListInTarget(){
         let UserInfo = {};
         UserInfo.api_token = this.state.api_token;
         UserInfo.userID = this.state.userID;
-        UserInfo.classID = this.state.classID;
-        UserInfo.pLevel = this.state.pLevel;
-        UserInfo.targetID = this.state.targetID;
-
-        _class.getStudentInTarget(UserInfo).then(res => {
-            this.setState({list : res.student_scores});
+      
+        _class.getStudentsInTaskPage(UserInfo).then(res => {
+            this.setState({list : res.tasks.details});
         }, errMsg =>{
             this.setState({ 
                 list : []
@@ -152,40 +89,20 @@ class TargetDetail extends React.Component{
        
                 return (
                     <div id="page-wrapper" style={{marginTop:"0px"}}>
-                        <h1 className="display-1" style={{fontWeight:"bold", color:"grey", opacity:"0.3", marginBottom:"50px"}}>Students</h1>
-                        <p>Select Module</p>
-                
-                            <select value={this.state.taskID} 
-                            onChange={this.handleChange}>
-                                <option>Pick one </option>
-                            {
-                            tasklist
-                            }
-                            </select> 
-                            <button className="btn btn-lg btn-primary btn-block"
-                            onClick={e => {this.onSubmit(e)}}>Assign Module</button>
+                        <h1 className="display-1" style={{fontWeight:"bold", color:"grey", opacity:"0.3", marginBottom:"50px"}}>Assigned Tasks</h1>
+                      
                         <PreLoader display="none" ref="loader" size=""></PreLoader>
                         
                         <div className="row">
                             { 
-                                this.state.list.map((student, index) => {
+                                this.state.list.map((modules, index) => {
                                     return (
                                         <div className="card col-md-3" key={index} style={{padding:"0px", marginLeft:"40px"}}>
                                             <div className="card-header" style={{backgroundColor:"#019DF4"}}>
                                             </div>
                                            
                                                 <div className="card-body" style={{backgroundColor:"#02D0FF"}}>
-                                                    <p className="display-3 text-white" style={{marginBottom:"0px", fontWeight:"bold"}}>{student.users.name}</p> 
-                                                    <div className="form-group">
-                                                        <input type="checkbox" 
-                                                            name="selected"
-                                                            value={student.user_id}
-                                                            className="form-control" 
-                                                            placeholder="checkbox" 
-                                                            onKeyUp={e => this.onInputKeyUp(e)}
-                                                            onChange={e => this.onInputChange(e)}
-                                                            style={{backgroundColor:"#4BDEFF", border:"none", color:"black"}}/>
-                                                    </div>
+                                                    <p className="display-3 text-white" style={{marginBottom:"0px", fontWeight:"bold"}}>{modules.name}</p> 
                                                 </div>
                                            
                                         </div>
